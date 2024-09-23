98f3e3075f80adbe5f400c1c0c7bf9431c7eb837
hdoll0 <HadeelAbed2000@outlook.com>
2024-09-23 19:19:04 +0300

    Category

---
+++ src/Controllers/Category.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Entity;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CareQuest.Controllers
+{
+
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private static List<Category> categories = new List<Category>();
+
+        [HttpGet]
+        public ActionResult<Category> GetCategories()
+        {
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult GetCategory(int id)
+        {
+            var category = categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public ActionResult GetCatogry(Category newCategory)
+        {
+            //categories.Add(newCategory);
+            //return Ok(newCategory);
+            return CreatedAtAction(nameof(GetCategory), new { id = newCategory.Id }, newCategory);
+        }
+
+        [HttpDelete("{id}")]
+
+        public ActionResult DeleteCategory(int id)
+        {
+            var category = categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            categories.Remove(category);
+            return NoContent();
+
+        }
+
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateCategory(int id, Category updatedCategory)
+        {
+            var category = categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            category.Name = updatedCategory.Name;
+            return Ok(category);
+
+        }
+    }
+}
+
+
