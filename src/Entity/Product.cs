725e8213d4a9962ace621440ff63abefa2e5fc23
shuruq25 <shroogad@gmail.com>
2024-09-24 15:10:01 +0300

    ..

---
+++ src/Entity/Product.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace src.Entity
+{
+    public class Product
+    {
+        
+        public int Id { get; set; } 
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public string? Description { get; set; }
+        public int cartId { get; set; }
+        public int categroyId{ get; set; }
+
+    }
+
