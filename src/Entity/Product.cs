67090e4fa01868ac5993f2190d6af4bc9bbe6eea
shuruq25 <shroogad@gmail.com>
2024-09-24 15:28:21 +0300

    Modified entity product.

---
+++ src/Entity/Product.cs
@@ -0,0 +1,14 @@
+namespace src.Entity
+{
+    public class Product
+    {
+
+        public int Id { get; set; } 
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public string? Description { get; set; }
+        public int cartId { get; set; }
+        public int categroyId{ get; set; }
+
+    }
+}
