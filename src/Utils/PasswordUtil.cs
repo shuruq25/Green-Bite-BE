baf1502cfaa0f34062258caa8156ac681b21ec32
Abdullah Khawahir <abdullah.khawahir@gmail.com>
2024-09-25 13:39:47 +0300

    add password util

---
+++ src/Utils/PasswordUtil.cs
@@ -0,0 +1,21 @@
+using System.Security.Cryptography;
+using System.Text;
+
+public class PasswordUtil
+{
+
+    public static string HashPassword(string originaPassword, out string HashedPassword, out byte[] salt)
+    {
+        var hasher = new HMACSHA256();
+        salt = hasher.Key;
+        HashedPassword = BitConverter.ToString(hasher.ComputeHash(Encoding.UTF8.GetBytes(originaPassword)));
+        return HashedPassword;
+    }
+
+    public static bool VerifyPassword(string originaPassword, string HashedPassword, byte[] salt)
+    {
+        var hasher = new HMACSHA256(salt);
+        return BitConverter.ToString(hasher.ComputeHash(Encoding.UTF8.GetBytes(originaPassword))) == HashedPassword;
+
+    }
+}
