f8683feb6470ad9fec5a1b3f8fb5e79d8f0b9bfa
Abdullah Khawahir <abdullah.khawahir@gmail.com>
2024-09-25 14:02:39 +0300

    add login logut to User controller

--- src/Utils/PasswordUtil.cs
+++ src/Utils/PasswordUtil.cs
@@ -1,7 +1,7 @@
 using System.Security.Cryptography;
 using System.Text;
 
-public class PasswordUtil
+public class PasswordUtils
 {
 
     public static string HashPassword(string originaPassword, out string HashedPassword, out byte[] salt)
