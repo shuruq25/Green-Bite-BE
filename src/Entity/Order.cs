80c325daa4f9c31695531bea2ebcec12b00423ee
Abdullah Khawahir <abdullah.khawahir@gmail.com>
2024-10-01 16:12:18 +0300

    Add relations betwenn order payment

--- src/Entity/Order.cs
+++ src/Entity/Order.cs
@@ -15,10 +15,12 @@ namespace src.Entity
     {
         public Guid ID { get; set; }
         public Guid UserID { get; set; }
+        public User User { get; set; }
         public decimal OriginalPrice { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime EstimatedArrival { get; set; }
         public OrderStatuses Status { get; set; }
-        public Guid PaymentID { get; set; }
+        public Guid? PaymentID { get; set; }
+        public Payment? Payment { get; set; }
     }
 }
